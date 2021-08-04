3a1f2d06c009cb19296d7849440b8f19c743fc3d
raquel <raquelanaroman@gmail.com>
2021-08-04 12:29:26 -0400

    wrap content in main html element and flex styling

--- src/App.js
+++ src/App.js
@@ -9,23 +9,25 @@ function App() {
   return (
     <div className="App">
       <Nav />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/about">
-          <h1>This is an about page</h1>
-        </Route>
-        <Route path="/how-to">
-          <h1>This is directions on how to use Raised</h1>
-        </Route>
-        <Route path="/existing-gardens">
-          <h1>This is all existing gardens</h1>
-        </Route>
-        <Route path="/plant-encyclopedia">
-          <h1>This is about plants</h1>
-        </Route>
-      </Switch>
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <h1>This is an about page</h1>
+          </Route>
+          <Route path="/how-to">
+            <h1>This is directions on how to use Raised</h1>
+          </Route>
+          <Route path="/existing-gardens">
+            <h1>This is all existing gardens</h1>
+          </Route>
+          <Route path="/plant-encyclopedia">
+            <h1>This is about plants</h1>
+          </Route>
+        </Switch>
+      </main>
     </div>
   );
 }
