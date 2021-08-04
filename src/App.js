c1e94420a6b9dc053d86d4a0137b7fee88a3b100
raquel <raquelanaroman@gmail.com>
2021-08-04 10:39:06 -0400

    remove unnecessary elements and add buttons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
-
-
+import Button from './button/Button';
 
 function App() {
 
+  function newGardenClick(){
+    console.log('create a new garden')
+  }
+
+  function existingGardenClick(){
+    console.log('existing gardens here')
+  }
+
   return (
     <div className="App">
-      <nav className="App-header">
-      </nav>
-      <div id='form-div' className="App-header">
+      <div id="intro-div">
+        <h1 id="intro-header">&#123; R A I S E D &#125;</h1>
+        <p id="intro-content">a raised-bed garden planning app</p>
+        <Button text="Create A New Garden" clickHandler={newGardenClick}/>
+        <Button text="Explore Existing Gardens" clickHandler={existingGardenClick}/>
       </div>
     </div>
   );
