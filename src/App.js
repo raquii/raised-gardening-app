5e5712cb36f31b3280121e5cdf22a72c589d0d9e
raquel <raquelanaroman@gmail.com>
2021-08-04 11:04:37 -0400

    add preliminary switch/routes for pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 import Nav from './nav/Nav';
 import Home from './home/Home';
 
 function App() {
-  
+
   return (
     <div className="App">
       <Nav />
-      <Home />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <h1>This is an about page</h1>
+        </Route>
+        <Route path="/how-to">
+          <h1>This is directions on how to use Raised</h1>
+        </Route>
+        <Route path="/existing-gardens">
+          <h1>This is all existing gardens</h1>
+        </Route>
+        <Route path="/plant-encyclopedia">
+          <h1>This is about plants</h1>
+        </Route>
+      </Switch>
     </div>
   );
 }
