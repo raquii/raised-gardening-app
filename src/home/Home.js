268a0985ee1803e054cfa185355e962171312424
raquel <raquelanaroman@gmail.com>
2021-08-04 15:44:46 -0400

    add garden form with conditional render to home pg

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,23 +1,34 @@
+import { useState } from 'react';
 import Button from '../button/Button';
+import GardenForm from '../garden-creator/GardenForm';
 import './Home.css';
 
 function Home() {
+  const [showForm, setShowForm] = useState(false);
+
   function newGardenClick() {
-    console.log('create a new garden')
+    setShowForm(!showForm)
   }
 
   function existingGardenClick() {
     console.log('existing gardens here')
   }
 
+  function buttonDiv() {
+    return (
+      <div className="button-container">
+        <Button text="Create a New Garden" clickHandler={newGardenClick} />
+        <Button text="Explore Existing Gardens" clickHandler={existingGardenClick} />
+      </div>
+    )
+  }
+
+
   return (
     <div id="intro-div">
       <h1 id="intro-header">&#123; R A I S E D &#125;</h1>
       <p id="intro-content">a raised-bed garden planning app</p>
-      <div id="button-container">
-        <Button text="Create a New Garden" clickHandler={newGardenClick} />
-        <Button text="Explore Existing Gardens" clickHandler={existingGardenClick} />
-      </div>
+      {showForm ? <GardenForm goBack={ newGardenClick }/> : buttonDiv()}
     </div>
   )
 }
