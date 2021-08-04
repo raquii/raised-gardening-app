9ae24b315888e8cd825ea71ea60a741c4f0f7f23
raquel <raquelanaroman@gmail.com>
2021-08-04 09:27:11 -0400

    create front end repo

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <nav className="App-header">
+      </nav>
+      <div id='form-div' className="App-header">
+      </div>
+    </div>
+  );
+}
+
+export default App;
