073631fa5d8ce9b41a1bd308c4ace8b48dafc76a
raquel <raquelanaroman@gmail.com>
2021-08-05 00:37:19 -0400

    move data into plant detail component

--- src/plantpedia/PlantCard.js
+++ src/plantpedia/PlantCard.js
@@ -1,80 +1,15 @@
-import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function PlantCard({
+    id,
     name,
-    category,
-    perSqFt,
-    daysTilHarvest,
-    plantingDate,
-    plantNote,
-    growNote,
-    harvestNote
 }) {
-    const [showMore, setShowMore] = useState(false);
 
-    function toggleShowMore() {
-        setShowMore(!showMore)
-    }
 
     return (
-        <div id={name} className="plant-card" style={showMore ? { width: '80%' } : { width: '30%' }}>
+        <div id={name} className="plant-card">
             <h2 className="plant-header">{name}</h2>
-            <table className="plant-stats">
-                <tbody>
-                    <tr>
-                        <th>Category: </th>
-                        <td>{category}</td>
-                    </tr>
-                    <tr>
-                        <th>Plants Per SqFt:</th>
-                        <td>{perSqFt}</td>
-                    </tr>
-                    <tr>
-                        <th>When to Plant: </th>
-                        <td>{plantingDate}</td>
-                    </tr>
-                    <tr>
-                        <th>Days Til Harvest: </th>
-                        <td>{daysTilHarvest}</td>
-                    </tr>
-                </tbody>
-            </table>
-
-            <button onClick={toggleShowMore}>
-                {showMore ?
-                    <i className="fa fa-angle-double-up" />
-                    :
-                    <i className="fa fa-angle-double-down" />
-                }
-            </button>
-
-            <div
-                className="plant-note-container"
-                style={showMore ?
-                    { display: '' }
-                    :
-                    { display: "none" }
-                }
-            >
-                <h4 className="plant-subheader">Notes:</h4>
-                <table className="plant-notes">
-                    <tbody>
-                        <tr>
-                            <th>Planting:</th>
-                            <td>{plantNote}</td>
-                        </tr>
-                        <tr>
-                            <th>Growing:</th>
-                            <td>{growNote}</td>
-                        </tr>
-                        <tr>
-                            <th>Harvesting:</th>
-                            <td>{harvestNote}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-
+            <Link to={`/plant-encyclopedia/${id}`}><i className="fa fa-info-circle"/></Link>
         </div>
     )
 }
