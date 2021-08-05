5655c1a4ac29f1bee177a938b3f0fe7a2a46bc14
raquel <raquelanaroman@gmail.com>
2021-08-05 10:02:27 -0400

    add description prop with styling

--- src/card/Card.js
+++ src/card/Card.js
@@ -5,12 +5,14 @@ function Card({
     id,
     link,
     name,
+    description
 }) {
 
 
     return (
         <div id={name} className="card">
             <h2 className="header">{name}</h2>
+            <span>{description}</span>
             <Link to={`/${link}/${id}`}><i className="fa fa-info-circle"/></Link>
         </div>
     )
