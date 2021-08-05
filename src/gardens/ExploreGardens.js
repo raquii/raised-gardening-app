353401f525ab4bb9b8f3501e238e942480d364b0
raquel <raquelanaroman@gmail.com>
2021-08-05 10:02:08 -0400

    fetch garden data and map & render to card comp

--- src/gardens/ExploreGardens.js
+++ src/gardens/ExploreGardens.js
@@ -1,5 +1,29 @@
-function ExploreGardens(){
+import { useState, useEffect } from "react";
+import Card from "../card/Card";
 
+function ExploreGardens() {
+    const [gardens, setGardens] = useState([]);
+    useEffect(() => {
+
+        fetch('http://localhost:9393/gardens')
+        .then(r => r.json())
+        .then(data => setGardens(data.gardens))
+
+    }, [])
+
+    const gardenCard = gardens.map(garden =>{
+        return <Card 
+            key={garden.name} 
+            id={garden.id} 
+            name={garden.name}
+            description={`${garden.length}'x${garden.width}'x${garden.depth}"`} />
+    })
+
+    return (
+        <div id='gardens-div'>
+            {gardenCard}
+        </div>
+    )
 }
 
 export default ExploreGardens;
