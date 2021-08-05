7dff4432059755073c9692ba6b9dc14ada865772
raquel <raquelanaroman@gmail.com>
2021-08-04 23:20:29 -0400

    add plantpedia component

---
+++ src/plantpedia/Plantpedia.js
@@ -0,0 +1,39 @@
+import './Plantpedia.css';
+import { useState } from 'react';
+import PlantCard from './PlantCard';
+
+function Plantpedia({ plants }) {
+    const [search, setSearch] = useState("");
+
+    const plantCards = plants.filter(plant=>plant.name.toUpperCase().includes(search.toUpperCase())).map(plant => {
+        return <PlantCard
+            key={plant.name}
+            name={plant.name}
+            category={plant.category}
+            perSqFt={plant.per_sq_ft}
+            plantingDate={plant.planting_dates}
+            daysTilHarvest={plant.days_to_harvest}
+            plantNote={plant.notes[0].content}
+            growNote={plant.notes[1].content}
+            harvestNote={plant.notes[2].content}
+        />
+    });
+
+    function handleSearchChange(e){
+        setSearch(e.target.value)
+    }
+
+    return (
+        <div id="plantpedia">
+            <div id="plantpedia-search">
+                <label>Search: </label>
+                <input type="text" value={search} onChange={handleSearchChange}/>
+            </div>
+            <div id="card-container">
+                {plantCards}
+            </div>
+        </div>
+    )
+}
+
+export default Plantpedia;
