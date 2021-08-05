d572a62afae3a90e8a1fe6e152c434f0811e5903
raquel <raquelanaroman@gmail.com>
2021-08-05 09:44:37 -0400

    remove unnecessary props from card comp

--- src/plantpedia/Plantpedia.js
+++ src/plantpedia/Plantpedia.js
@@ -1,22 +1,16 @@
 import './Plantpedia.css';
 import { useState } from 'react';
-import PlantCard from './PlantCard';
+import Card from '../card/Card';
 
 function Plantpedia({ plants }) {
     const [search, setSearch] = useState("");
 
     const plantCards = plants.filter(plant => plant.name.toUpperCase().includes(search.toUpperCase())).map(plant => {
-        return <PlantCard
+        return <Card
             key={plant.name}
             id={plant.id}
             name={plant.name}
-            category={plant.category}
-            perSqFt={plant.per_sq_ft}
-            plantingDate={plant.planting_dates}
-            daysTilHarvest={plant.days_to_harvest}
-            plantNote={plant.notes[0].content}
-            growNote={plant.notes[1].content}
-            harvestNote={plant.notes[2].content}
+            link={`plant-encyclopedia`}
         />
     });
 
